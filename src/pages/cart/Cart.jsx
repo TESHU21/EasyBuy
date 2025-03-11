eec0fdd3b0e894425105fb45f1f5eb2feb33c360
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-03-11 21:51:04 +0300

    Add Breadcrumb to Cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import {useCart} from '@/context/cartContext'
 import { Button } from '@/components/ui/button'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
 
 
 const Cart = () => {
     const {addToCart,removeFromCart,clearCart,getCartTotal,cartItems} = useCart()
   return (
-    <div className='p-2'>
-        <h1>Shoping Cart</h1>
+    <div className="flex flex-col">
+      <Breadcrumb className="flex justify-center">
+         <BreadcrumbList>
+           <BreadcrumbItem>
+             <BreadcrumbLink href="/">Home</BreadcrumbLink>
+           </BreadcrumbItem>
+           <BreadcrumbSeparator />
+           <BreadcrumbItem>
+             <BreadcrumbPage>Cart</BreadcrumbPage>
+           </BreadcrumbItem>
+         </BreadcrumbList>
+       </Breadcrumb>
         <div>
            
             {cartItems.length===0? (
