e89de0e0bc606cbc534854b93775e27aee116da1
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-03-11 10:42:08 +0300

    add search product by name functionality to shop page

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -9,8 +9,16 @@ import {
   } from "@/components/ui/breadcrumb"
   import ProductItem from '../products/components/ProductItem'
   import { products } from "../../assets/hero/assets"
+  import { Input } from '@/components/ui/input'
   
 const Shop = () => {
+    const [filterdProducts,setFilterdProducts] = useState(products)
+    const [searchValue,setSearchValue] = useState("")
+   useEffect(()=>{
+    const filtered= products.filter((product)=> product.name.toLowerCase().includes(searchValue.toLowerCase()))
+    setFilterdProducts(filtered)
+
+   },[searchValue])
   return (
     <div>
         <Breadcrumb className="flex justify-center">
@@ -24,9 +32,10 @@ const Shop = () => {
     </BreadcrumbItem>
   </BreadcrumbList>
 </Breadcrumb>
-<div>
+<div className='flex flex-col items-center pt-10'>
+    <Input type='text' placeholder='Search Products' className='w-1/3 px-4 py-6' onChange={(e)=> setSearchValue(e.target.value)}/>  
     <div className='grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4 gap-4 p-4'>
-        {products.map((product,index)=>(
+        {filterdProducts.map((product,index)=>(
             <ProductItem key={index} products={product} />
         ))}
        
